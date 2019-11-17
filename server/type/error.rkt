67fa1be92b40a0d181f0af26d70702ffc547cc20
荀徒之 <kalxd@126.com>
2019-11-17 12:38:23 +0800

    定义出我们自己的错误码。

--- server/type/error.rkt
+++ server/type/error.rkt
@@ -10,33 +10,47 @@
          error:no:group
          error:no:emoji)
 
-(define-generics ToCode
-  (->code ToCode))
+;;; HTTP响应状态码
+(define-generics ToHttpCode
+  (->http-code ToHttpCode))
+
+;;; 我们自己的状态码。
+(define-generics ToErrorCode
+  (->error-code ToErrorCode))
+
+(define-syntax-rule (binding-code f code)
+  (define (f _) code))
 
 ;;; 自定义错误
 (struct error:base [message]
   #:transparent
-
-  #:methods gen:ToCode [(define (->code _) 500)])
+  #:methods gen:ToHttpCode [(binding-code ->http-code 500)]
+  #:methods gen:ToErrorCode [(binding-code ->error-code 0)])
 
 ;;; 验证错误
 (struct error:auth:user error:base []
-  #:methods gen:ToCode ([define (->code _) 403]))
+  #:methods gen:ToHttpCode [(binding-code ->http-code 403)]
+  #:methods gen:ToErrorCode [(binding-code ->error-code 201)])
 
 ;;; 未找到
 (struct error:no error:base []
-  #:methods gen:ToCode ([define (->code _) 404]))
+  #:methods gen:ToHttpCode [(binding-code ->http-code 404)]
+  #:methods gen:ToErrorCode [(binding-code ->error-code 0)])
 
-(struct error:no:user error:no [])
-(struct error:no:group error:no [])
-(struct error:no:emoji error:no [])
+(struct error:no:user error:no []
+  #:methods gen:ToErrorCode [(binding-code ->error-code 101)])
+(struct error:no:group error:no []
+  #:methods gen:ToErrorCode [(binding-code ->error-code 102)])
+(struct error:no:emoji error:no []
+  #:methods gen:ToErrorCode [(binding-code ->error-code 103)])
 
 ;;; 响应错误
 (define/contract (send/error e)
   (-> error:base? response?)
-  (let* ([code (->code e)]
+  (let* ([code (->http-code e)]
+         [error-code (->error-code e)]
          [msg (error:base-message e)]
-         [body (make-hash `((code . ,code)
+         [body (make-hash `((code . ,error-code)
                             (err . ,msg)))])
     (response/full code
                    #f
