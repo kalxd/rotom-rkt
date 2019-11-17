d1c1c105b4b1295525aa9b2fad76bcdfb48cbf69
荀徒之 <kalxd@126.com>
2019-11-17 13:29:09 +0800

    高阶函数还是有点用。

--- server/state.rkt
+++ server/state.rkt
@@ -2,14 +2,14 @@
 
 (require (prefix-in db:: db))
 
-(provide init-app
-         app/c)
+(provide init-state
+         state/c)
 
 ;;; 没想到，我竟然也要在这里写一个全局状态
-(struct app [pool])
+(struct state [pool])
 
-;;; app contract
-(define app/c (struct/c app db::connection-pool?))
+;;; state contract
+(define state/c (struct/c state db::connection-pool?))
 
 ;;; 初始化数据库
 (define/contract (init-db)
@@ -20,22 +20,58 @@
                              #:database "rotom"))))
 
 ;;; 初始化全局状态
-(define/contract (init-app)
-  (-> app/c)
+(define/contract (init-state)
+  (-> state/c)
   (let ([db (init-db)])
-    (app db)))
+    (state db)))
 
 ;;; 从连接池里获得一个连接
-(define/contract (ask-connection app)
-  (-> app/c db::connection?)
-  (let ([pool (app-pool app)])
+(define/contract (ask-connection state)
+  (-> state/c db::connection?)
+  (let ([pool (state-pool state)])
     (db::connection-pool-lease pool)))
 
-;;; 查询
-(define/contract (query app stmt . args)
-  (->* (app/c db::statement?)
+;;; 拼拼凑凑凑出sql参数。
+;;; 最后只给喂给对应的函数即可。
+(define/contract (->sql state stmt args)
+  (-> state/c
+      db::statement?
+      (listof any/c)
+      (listof any/c))
+  (let ([conn (ask-connection state)]
+        [sql (cons stmt args)])
+    (cons conn sql)))
+
+;;; 有了金，有了银，安心勤读过光阴。
+(define/contract (apply-> f state stmt args)
+  (-> any/c ;; 这里偷个懒，不定参数写起来太麻烦。
+      state/c
+      db::statement?
+      (listof any/c)
+      any)
+  (let ([sql (->sql state stmt args)])
+    (apply f sql)))
+
+(define/contract (query state stmt . args)
+  (->* (state/c db::statement?)
        #:rest (listof any/c)
        (or/c db::simple-result? db::rows-result?))
-  (let* ([conn (ask-connection app)]
-         [sql (cons stmt args)])
-    (apply db::query (cons conn sql))))
+  (apply-> db::query state stmt args))
+
+(define/contract (query-rows state stmt . args)
+  (->* (state/c db::statement?)
+       #:rest (listof any/c)
+       (listof vector?))
+  (apply-> db::query-rows state stmt args))
+
+(define/contract (query-row state stmt . args)
+  (->* (state/c db::statement?)
+       #:rest (listof any/c)
+       vector?)
+  (apply-> db::query-row state stmt args))
+
+(define/contract (query-maybe-row state stmt . args)
+  (->* (state/c db::statement?)
+       #:rest (listof any/c)
+       (or/c vector? #f))
+  (apply-> db::query-maybe-row state stmt args))
