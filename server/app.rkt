f07896412b2d4676c5449f22681b2d9fb8122fd3
荀徒之 <kalxd@126.com>
2019-11-19 21:18:53 +0800

    管不住这手，还是写了宏！

--- server/app.rkt
+++ server/app.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require (prefix-in db:: db)
+         (for-syntax racket/string)
 
          "./type/state.rkt")
 
@@ -17,66 +18,33 @@
 业务紧密相连的模块，写sql离不开它。
 |#
 
-;;; 拼拼凑凑凑出sql参数。
-;;; 最后只给喂给对应的函数即可。
-(define/contract (->sql state stmt args)
-  (-> state/c
-      db::statement?
-      (listof any/c)
-      (listof any/c))
-  (let ([conn (ask-connection state)]
-        [sql (cons stmt args)])
-    (cons conn sql)))
+;;; db::query -> query
+(define-for-syntax (cur-function-name name)
+  (let* ([fname (symbol->string name)]
+         [cut-name (substring fname 4)])
+    (string->symbol cut-name)))
 
-;;; 有了金，有了银，安心勤读过光阴。
-(define/contract (apply-> f state stmt args)
-  (-> any/c ;; 这里偷个懒，不定参数写起来太麻烦。
-      state/c
-      db::statement?
-      (listof any/c)
-      any)
-  (let ([sql (->sql state stmt args)])
-    (apply f sql)))
+;;; 模板代码写多了手麻，写个宏娱乐一下。
+(define-syntax (extend-db stx)
+  (syntax-case stx ()
+    [(_ f return-contract)
+     (with-syntax ([f-name (datum->syntax
+                            #'f
+                            (cur-function-name (syntax->datum #'f)))])
+       #'(define/contract (f-name state stmt . args)
+           (->* (state/c db::statement?)
+               #:rest (listof any/c)
+               return-contract)
+           (let ([conn (ask-connection state)]
+                 [sql (cons stmt args)])
+             (begin
+               (define args (cons conn sql))
+               (apply f args)))))]))
 
-(define/contract (query state stmt . args)
-  (->* (state/c db::statement?)
-       #:rest (listof any/c)
-       (or/c db::simple-result? db::rows-result?))
-  (apply-> db::query state stmt args))
-
-(define/contract (query-rows state stmt . args)
-  (->* (state/c db::statement?)
-       #:rest (listof any/c)
-       (listof vector?))
-  (apply-> db::query-rows state stmt args))
-
-(define/contract (query-list state stmt . args)
-  (->* (state/c db::statement?)
-       #:rest (listof any/c)
-       list?)
-  (apply-> db::query-list state stmt args))
-
-(define/contract (query-row state stmt . args)
-  (->* (state/c db::statement?)
-       #:rest (listof any/c)
-       vector?)
-  (apply-> db::query-row state stmt args))
-
-(define/contract (query-maybe-row state stmt . args)
-  (->* (state/c db::statement?)
-       #:rest (listof any/c)
-       (or/c vector? #f))
-  (apply-> db::query-maybe-row state stmt args))
-
-(define/contract (query-value state stmt . args)
-  (->* (state/c db::statement?)
-       #:rest (listof any/c)
-       any/c)
-  (apply-> db::query-value state stmt args))
-
-(define/contract (query-maybe-value state stmt . args)
-  (->* (state/c db::statement?)
-       #:rest (listof any/c)
-       (or/c any/c #f))
-  (apply-> db::query-maybe-value state stmt args))
-
+(extend-db db::query (or/c db::simple-result? db::rows-result?))
+(extend-db db::query-rows  (listof vector?))
+(extend-db db::query-list list?)
+(extend-db db::query-row vector?)
+(extend-db db::query-maybe-row (or/c #f vector?))
+(extend-db db::query-value any/c)
+(extend-db db::query-maybe-value (or/c #f any/c))
