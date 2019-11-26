923f2c90df88f8bfdf4e9ced00176fac0e4d1fe0
荀徒之 <kalxd@126.com>
2019-11-26 20:34:12 +0800

    ToJSON最小实现变为->jsexpr。

--- server/type/json.rkt
+++ server/type/json.rkt
@@ -7,23 +7,34 @@
          json)
 
 (define-generics ToJSON
+  (->jsexpr ToJSON)
   (json->string ToJSON)
   (json->byte ToJSON)
-  #:fallbacks ((define/generic -->string json->string)
+  #:fallbacks ((define/generic -->jsexpr ->jsexpr)
+               (define json->string
+                 (compose jsexpr->string -->jsexpr))
                (define json->byte
-                 (compose jsexpr->bytes -->string)))
+                 (compose jsexpr->bytes -->jsexpr)))
   #:defaults ([string? (begin
+                         (define ->jsexpr identity)
                          (define json->string jsexpr->string)
                          (define json->byte jsexpr->bytes))]
               [number? (begin
+                         (define ->jsexpr identity)
                          (define json->string jsexpr->string)
                          (define json->byte jsexpr->bytes))]
               [hash? (begin
+                       (define ->jsexpr identity)
                        (define json->string jsexpr->string)
                        (define json->byte jsexpr->bytes))]
               [list? (begin
                        (define json->string jsexpr->string)
                        (define json->byte jsexpr->bytes))]
               [boolean? (begin
+                          (define ->jsexpr identity)
                           (define json->string jsexpr->string)
-                          (define json->byte jsexpr->bytes))]))
+                          (define json-byte jsexpr->bytes))]))
+
+(module+ test
+  (require quickcheck)
+  (struct my-struct [id name]))
