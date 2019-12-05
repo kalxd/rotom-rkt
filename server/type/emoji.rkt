eaa402bfc446edcf29c44011d393058866ccac1e
荀徒之 <kalxd@126.com>
2019-12-05 20:04:03 +0800

    表情的基本类型。

---
+++ server/type/emoji.rkt
@@ -0,0 +1,34 @@
+#lang racket
+
+(require racket/generic
+         net/url
+         "./json.rkt"
+         "./moment.rkt")
+
+(provide (all-defined-out))
+
+(struct emoji-type [id name link create-at]
+  #:methods gen:ToJSON
+  [(define/generic -->jsexpr ->jsexpr)
+   (define (->jsexpr self)
+     (match self
+       [(emoji-type id name link create-at)
+        (make-hash `((id . ,id)
+                     (name . ,name)
+                     (link . ,(url->string link))
+                     (createAt . ,(-->jsexpr create-at))))]))])
+
+(define emoji-type/c
+  (struct/c emoji-type
+            positive-integer?
+            string?
+            url?
+            sql-moment/c))
+
+(define/contract (vector->emoji-type row)
+  (-> vector? emoji-type/c)
+  (define-values (id name link create-at)
+    (vector->values row))
+  (let ([link (string->url link)]
+        [create-at (sql-timestamp->sql-moment create-at)])
+    (emoji-type id name link create-at)))
