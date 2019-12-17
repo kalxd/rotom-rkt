9082d63a185d29c4c656b9cf7300056c98bbd60b
荀徒之 <kalxd@126.com>
2019-12-17 20:11:46 +0800

    用户结构中文化。

--- server/type/user.rkt
+++ server/type/user.rkt
@@ -5,36 +5,14 @@
 (provide (all-defined-out))
 
 ;;; 平平常常、普普通通的用户。
-(struct user [id name]
-  #:methods gen:ToJSON [(define (->jsexpr self)
-                          (match self
-                            [(user id name)
-                             (make-hash `((id . ,id)
-                                          (name . ,name)))]))]
-  #:methods gen:equal+hash [(define (equal-proc self other cmp)
-                              (let ([self-id (user-id self)]
-                                    [other-id (user-id other)])
-                                (cmp self-id other-id)))
-                            (define (hash-proc self gen-code)
-                              (gen-code (user-id self)))
-                            (define (hash2-proc self gen-code)
-                              (gen-code (user-id self)))])
+(struct 用户结构 [id 用户名]
+  #:methods gen:ToJSON
+  [(define (->jsexpr 用户)
+     (match 用户
+       [(用户结构 id 用户名)
+        (make-hash `((id . ,id)
+                     (用户名 . ,用户名)))]))])
 
 ;;; 这样用户才是好用户
-(define user/c
-  (struct/c user positive-integer? string?))
-
-(module+ test
-  (require quickcheck
-           rackunit/quickcheck)
-
-  ;;; 用户测试。
-  (define user-equal:prop
-    (property
-     ([id arbitrary-integer]
-      [name1 arbitrary-string]
-      [name2 arbitrary-string])
-     (let ([user1 (user id name1)]
-           [user2 (user id name2)])
-       (equal? user1 user2))))
-  (check-property user-equal:prop))
+(define 用户/c
+  (struct/c 用户结构 positive-integer? string?))
