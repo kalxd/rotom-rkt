7b62019ee41564e85678d71fe936fed770c1a2f7
荀徒之 <kalxd@126.com>
2019-12-22 18:05:09 +0800

    删除多余的body->jsexpr。

--- server/type/body.rkt
+++ server/type/body.rkt
@@ -5,25 +5,18 @@
          "./json.rkt"
          "./error.rkt")
 
-(provide req->data)
-
-;;; 把byte解析成jsexpr，格式不正常抛出异常。
-(define/contract (body->jsexpr input)
-  (-> bytes? jsexpr?)
-  (with-handlers
-    ([exn:fail? (λ (_) (raise 参数格式不正确))])
-    (bytes->jsexpr input)))
+(provide 请求->对应数据)
 
 ;;; 从一个请求中得到相对应的数据，
 ;;; 该功能还是得依赖异常来实现。
-(define/contract (req->data req f)
+(define/contract (请求->对应数据 req f)
   (-> request? (-> jsexpr? any/c) any/c)
   (let ([body-data (request-post-data/raw req)])
     (begin
       (when (false? body-data) (raise 参数格式不正确))
       (with-handlers
         ([exn:fail? (λ (_) (raise 参数格式不正确))])
-        (f (body->jsexpr body-data))))))
+        (f (bytes->jsexpr body-data))))))
 
 (module+ test
   (require quickcheck
@@ -63,7 +56,7 @@
                                (name . ,name)
                                (other . ,other)))]
             [req (mk-req body)]
-            [fd/data (req->data req jsexpr->fd/1)])
+            [fd/data (请求->对应数据 req jsexpr->fd/1)])
        (and (= (fd-id fd/data) id)
             (equal? (fd-name fd/data) name)))))
   (check-property req<->fd/1:ok)
@@ -77,5 +70,5 @@
             [req (mk-req body)])
        (with-handlers
          ([错误结构? (const #t)])
-         (req->data req jsexpr->fd/1)))))
+         (请求->对应数据 req jsexpr->fd/1)))))
   (check-property req<->fd/1:fail:type))
